dc2ec21b95d0127ec1c587a69a7480530e90ec25
devsoft91 <interbelt@gmail.com>
2016-05-04 12:33:36 +0200

    Nuova configurazione server_app

--- server_app.js
+++ server_app.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 
 //app.use('/home',require('./controllers/home'));
-app.route('/login').get(function(req,res,next){
-	res.sendFile('./views/login.html', {root: __dirname });
-});
-//app.use('/register',require('./controllers/register'));
 
+app.route('/Quizzipedia/signup').get(function(req,res,next){
+	res.sendFile('./public/view/signup.html', {root: __dirname });
+});
+
+//app.use('/register',require('./controllers/register'));
+/*
 app.get('/', function (req, res) {
   return res.redirect('/login');
-});
+});*/
 
 app.listen(8080);

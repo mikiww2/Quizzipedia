814af1921aebadee7f1343ebb81c5d1ba9b29241
devsoft91 <interbelt@gmail.com>
2016-05-04 09:41:21 +0200

    Prima struttura cartelle

---
+++ app/angular_app.js
@@ -0,0 +1,6 @@
+var app = angular.module('Quizzipedia', []);
+
+app.controller('home_ctrl', function() {
+  var self = this;
+  self.name = 'Ciccio';
+}); 
---
+++ server_app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+//app.use('/home',require('./controllers/home'));
+app.route('/login').get(function(req,res,next){
+	res.sendFile('./views/login.html', {root: __dirname });
+});
+//app.use('/register',require('./controllers/register'));
+
+app.get('/', function (req, res) {
+  return res.redirect('/login');
+});
+
+app.listen(3030);

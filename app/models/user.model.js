51688bc1fc90de7469ea3cb09c40efda8671d532
miki2w <mikiww2@gmail.com>
2016-05-06 12:49:37 +0200

    aggiornato in base a varie decisioni prese

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -7,71 +7,43 @@
 // use me like this
 // var User = require('./user.model', { root: __dirname })(mongoose);
 
-var utility = require('./utility', { root: __dirname }); 
-
 module.exports = function(mongoose) {
+    var Schema = mongoose.Schema;
 
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-    firstName: {
-        type: String
-        ,trim: true
-        ,required: [true, 'firstName is required']
-//         ,validate: [validateLocalStrategyProperty, 'Please fill in your first name']
-    }
-    ,lastName: {
-        type: String
-        ,trim: true
-        ,required: [true, 'lastName is required']
-//         ,validate: [validateLocalStrategyProperty, 'Please fill in your last name']
-    }
-    ,_id: { // email
-        type: String
-        ,set: utility.lower
-        ,trim: true
-//         ,validate: [validateLocalStrategyProperty, 'Please fill in your email']
-        ,match: [/.+\@.+\..+/, 'Please fill a valid mail address, it\'s used like a index']
-    }
-    ,password: {
-        type: String
-        ,required: [true, 'password is required']
-    }
-    ,type: {
-        type: String
-        ,enum: ['NoRole', 'Director', 'Student', 'Teacher']
-        ,default: 'NoRole'
-        ,required: [true, 'type is required']
-    }
+    var userSchema = new Schema({
+        _id: { // email
+            type: String
+            ,lowercase: true
+            ,trim: true
+            ,match: [/.+\@.+\..+/, 'Please fill a valid mail address, it\'s used like a index']
+        }
+        ,firstName: {
+            type: String
+            ,trim: true
+            ,required: [true, 'firstName is required']
+        }
+        ,lastName: {
+            type: String
+            ,trim: true
+            ,required: [true, 'lastName is required']
+        }
+        ,password: {
+            type: String
+            ,required: [true, 'password is required']
+        }
+        ,typeUser: {
+            type: String
+            ,enum: ['NoRole', 'Director', 'Student', 'Teacher']
+            ,default: 'NoRole'
+            ,required: [true, 'typeUser is required']
+        }
     }, {
         strict: true
-        ,toObject: {
-            virtuals: true
-        }
-        ,toJSON: {
-            virtuals: true 
-        }
     });
 
-    userSchema.virtual('name.full').get(function () {
+    userSchema.virtual('fullName').get(function () {
         return this.name.first + ' ' + this.name.last;
     });
-    
-    userSchema.methods.getFirstName = function () {
-        return this.firstName;
-    }
-    
-    userSchema.methods.getLastName = function () {
-        return this.lasttName;
-    }
-    
-    userSchema.methods.getMail = function () {
-        return this.mail;
-    }
-    
-    userSchema.methods.getType = function () {
-        return this.type;
-    }
     
     userSchema.methods.checkPassword = function (password) {
         if(this.password == password)

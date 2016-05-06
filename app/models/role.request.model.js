3bd833fdeb776422ef4212d4b208ae8077f9b9b6
miki2w <mikiww2@gmail.com>
2016-05-06 20:02:58 +0200

    aggiunti model delle richieste

---
+++ app/models/role.request.model.js
@@ -0,0 +1,26 @@
+'use strict'; //will load js in strict mode
+
+//define the model for RoleRequest using RoleRequestSchema
+// IN var mongoose = require('mongoose');
+//OUT mongoose.model('RoleRequest', roleRequestSchema);
+
+// use me like this
+// var RoleRequest = require('./role.request.model', { root: __dirname })(mongoose);
+module.exports = function(mongoose) {
+
+    var Schema = mongoose.Schema;
+    
+    var roleRequestSchema = new Schema({
+        user: {
+            type: String
+            ,required: [true, 'user is required']
+        }
+        ,role: {
+            type: String
+            ,required: [true, 'role is required']
+            ,enum: ['Student','teacher']
+        }
+    }, { strict: true });
+
+    return  mongoose.model('RoleRequest', roleRequestSchema);
+}

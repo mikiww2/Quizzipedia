21a873c2271e7fd3cc09348e4a25e4b1b68948f6
devsoft91 <interbelt@gmail.com>
2016-05-11 10:45:50 +0200

    Adattato model per inclusione in più punti dell'app

--- app/model/user.model.js
+++ app/model/user.model.js
@@ -1,5 +1,6 @@
 'use strict'; //will load js in strict mode
 
+var mongoose = require('mongoose');
 //define the model for User using userSchema
 // IN var mongoose = require('mongoose');
 //OUT mongoose.model('User', userSchema);
@@ -7,50 +8,48 @@
 // use me like this
 // var User = require('./user.model', { root: __dirname })(mongoose);
 
-module.exports = function(mongoose) {
-    var Schema = mongoose.Schema;
+  var Schema = mongoose.Schema;
 
-    var userSchema = new Schema({
-        _id: { // email
-            type: String
-            ,lowercase: true
-            ,trim: true
-            ,match: [/.+\@.+\..+/, 'Please fill a valid mail address, it\'s used like a index']
-        }
-        ,firstName: {
-            type: String
-            ,trim: true
-            ,required: [true, 'firstName is required']
-        }
-        ,lastName: {
-            type: String
-            ,trim: true
-            ,required: [true, 'lastName is required']
-        }
-        ,password: {
-            type: String
-            ,required: [true, 'password is required']
-        }
-        ,typeUser: {
-            type: String
-            ,enum: ['NoRole', 'Director', 'Student', 'Teacher']
-            ,default: 'NoRole'
-            ,required: [true, 'typeUser is required']
-        }
-    }, {
-        strict: true
-    });
+  var userSchema = new Schema({
+      _id: { // email
+          type: String
+          ,lowercase: true
+          ,trim: true
+          ,match: [/.+\@.+\..+/, 'Please fill a valid mail address, it\'s used like a index']
+      }
+      ,firstName: {
+          type: String
+          ,trim: true
+          ,required: [true, 'firstName is required']
+      }
+      ,lastName: {
+          type: String
+          ,trim: true
+          ,required: [true, 'lastName is required']
+      }
+      ,password: {
+          type: String
+          ,required: [true, 'password is required']
+      }
+      ,typeUser: {
+          type: String
+          ,enum: ['NoRole', 'Director', 'Student', 'Teacher']
+          ,default: 'NoRole'
+          ,required: [true, 'typeUser is required']
+      }
+  }, {
+      strict: true
+  });
 
-    userSchema.virtual('fullName').get(function () {
-        return this.name.first + ' ' + this.name.last;
-    });
-    
-    userSchema.methods.checkPassword = function (password) {
-        if(this.password == password)
-            return true;
-        else
-            return false;
-    }
-    
-    return  mongoose.model('User', userSchema);
+userSchema.virtual('fullName').get(function () {
+    return this.name.first + ' ' + this.name.last;
+});
+
+userSchema.methods.checkPassword = function (password) {
+    if(this.password == password)
+        return true;
+    else
+        return false;
 }
+
+module.exports = mongoose.model('User', userSchema);

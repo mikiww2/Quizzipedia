3c8ce4fea74979e26d5d7d5891334b32f1139373
devsoft91 <interbelt@gmail.com>
2016-05-27 12:22:25 +0200

    [server] Aggiunto API

--- server.js
+++ server.js
@@ -30,6 +30,12 @@ app.use(cookieParser());
 // routes ======================================================================
 require('./app/route/api_route/authApi.js')(app);  // RESTful requests
 require('./app/route/api_route/profileApi.js')(app);  // RESTful requests
+require('./app/route/api_route/classApi.js')(app);  // RESTful requests
+require('./app/route/api_route/questionApi.js')(app);  // RESTful requests
+require('./app/route/api_route/quizApi.js')(app);  // RESTful requests
+require('./app/route/api_route/requestsApi.js')(app);  // RESTful requests
+require('./app/route/api_route/searchApi.js')(app);  // RESTful requests
+
 require('./app/route/route.js')(app);  // PAGES requests
 
 app.get('/Quizzipedia/aut', function (req, res) {

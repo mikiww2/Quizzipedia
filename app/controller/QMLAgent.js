1d6b627e0dbd8f2f10ff4b3058243fbd290db7c3
ftess <filippo90t@gmail.com>
2016-05-30 07:31:52 +0800

    aggiornato QMLAgent

--- app/controller/QMLAgent.js
+++ app/controller/QMLAgent.js
@@ -3,72 +3,122 @@
 // type = integer che indica il tipo (manca dal form )
 // text = stringa col testo della domanda (title)
 // answer = contiene le varie risposte ( da definire meglio in base al tipo di domanda, manca)
+// mancano le regole per allegati e simili
+
 exports.generate = function (question){
-    var stringType = '|q?' + question.type + '#';
-    var stringQuestion = question.txt + '#';
+    var stringType = '|q?' + question.type + '#t#';
+    var stringQuestion = question.txt + '#a#'; // da aggiungere nelle funzioni specifiche
     var stringAnswers;
 
     switch (question.type){ // in base al tipo fa operare la funzione corrispondente nella stringa
         case '1': // tipo vero/falso
-            stringAnswers = answerTF(question.ans);
+            stringAnswers = generateTF(question);
             break;
         case '2': // tipo risp multipla
-            stringAnswers = answerRM(question.ans);
+            stringAnswers = generateRM(question);
             break;
         case '3': // tipo a completamento
-            stringAnswers = answerCM(question.ans);
+            stringAnswers = generateCM(question);
             break;
         case '4': // tipo risp aperta
-            stringAnswers = answerRA(question.ans);
+            stringAnswers = generateRA(question);
             break;
         case '5': // tipo a collegamenti
-            stringAnswers = answerCL(question.ans);
+            stringAnswers = generateCL(question);
             break;
 
         // INSERIRE QUI I CASE PER LE NUOVE DOMANDE
 
         default:
-            return 'error!';
+            return 'error! question type not supported by generator';
     }
 
     var result = stringType + stringQuestion + stringAnswers + '|';
     return result;
 };
 
-exports.parse(qml){
+exports.parse = function (qml){
     if (qml.startsWith('|') && qml.endsWith('|')) {
-        var type = qml.charAt(3);
-        var text;
-        var answer;
+        var type = qml.charAt(qml.indexOf('q?')+2);
+        var qson;
+        switch (type){ // in base al tipo fa operare la funzione corrispondente nella stringa
+            case '1': // tipo vero/falso
+                qson = parserTF(qml);
+                break;
+            case '2': // tipo risp multipla
 
-        var qson = {'type': type};
+                break;
+            case '3': // tipo a completamento
+
+                break;
+            case '4': // tipo risp aperta
+                qson = parserRA(qml);
+                break;
+            case '5': // tipo a collegamenti
+
+                break;
+
+            // INSERIRE QUI I CASE PER LE NUOVE DOMANDE
+
+            default:
+                return 'error! question type not supported by parser';
+        }
+        //qson = {'type': type, 'text': text, 'ans': answer};
         return qson;
     }
     else
         throw 'qml syntax error';
 };
 
-// funzioni per la creazione di stringa di risposta specifica per ogni tipo
+// funzioni per la generazione di stringa di risposta specifica per ogni tipo
 
-var answerTF = function (answer) { //teoricamente ok
-    return answer + '#';
-}
+var generateTF = function (question) { //teoricamente ok
+    return question.ans + '#e#';
+};
 
-var answerRM = function (answer) { // da sistemare quando il form sarà funzionante
+var generateRM = function (question) { // da sistemare quando il form sarà funzionante
     var stringAnswers = '';
-    for (var item of answer)
+    for (var item of question.ans)
         stringAnswers = '§' + stringAnswers + item;
     return stringAnswers;
-}
+};
 
-var answerCM = function (answer) { // da sistemare quando il form sarà funzionante
-    return answer + '#';
-}
+var generateCM = function (question) { // da sistemare quando il form sarà funzionante
+    return question.ans + '#';
+};
 
-var answerRA = function (answer) {  // teoricamente ok
-    return answer + '#';
-}
+var generateRA = function (question) {  // teoricamente ok
+    return question.ans + '#e#';
+};
 
-var answerCL = function (answer) { // da sistemare quando il form sarà funzionante
-    return answer + '#';
-}
+var generateCL = function (question) { // da sistemare quando il form sarà funzionante
+    return question.ans + '#';
+};
+
+// funzioni per il parsing del qml specifico per tipo
+
+var parserTF = function (qml) { // teoricamente ok
+    var type = qml.charAt(qml.indexOf('q?')+2);
+    var text = qml.slice(qml.indexOf('#t#') + 3, qml.indexOf('#a#'));
+    var answer = qml.slice(qml.indexOf('#a#') + 3, qml.indexOf('#e#'));
+    return {'type': type, 'txt': text, 'ans': answer};
+};
+
+var parserRM = function (qml) { // da sistemare con form funzionante
+
+};
+
+var parserCM = function (qml) { // // da sistemare con form funzionante
+
+};
+
+var parserRA = function (qml) { // teoricamente ok
+    var type = qml.charAt(qml.indexOf('q?')+2);
+    var text = qml.slice(qml.indexOf('#t#') + 3, qml.indexOf('#a#'));
+    var answer = qml.slice(qml.indexOf('#a#') + 3, qml.indexOf('#e#'));
+    return {'type': type, 'txt': text, 'ans': answer};
+};
+
+var parserCL = function (qml) { // da sistemare con form funzionante
+
+};

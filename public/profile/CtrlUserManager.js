28954ee029c393500dc40a4942151a61c69e7123
BikuM <vassiliki.menarin@gmail.com>
2016-05-29 23:12:24 +0200

    Aggiornamento pagine utente

--- public/profile/CtrlUserManager.js
+++ public/profile/CtrlUserManager.js
@@ -1,40 +1,44 @@
-angular.module('profilemanager',[]).controller('CtrlUserManager',['$scope','$http',function($scope,$http){
+angular.module('ProfileManager').controller('CtrlUserManager',['$scope','$http',function($scope,$http){
     
-    $scope.user = {};
-    
+    $scope.user = null;
+    $scope.institutions = { 'Ancora in nessun ente' : 'Nessun ruolo assegnato' };
     $scope.oldPsw = null;
     $scope.newPsw = null;
     $scope.cNewPsw = null;
     $scope.recovering = false;  //indica se è un cambio pswd normale o un recuperoPswd
     $scope.emptystring = "";  //necessaria in caso cambio pswd normale
-
-    $http.get('/api/profile/get_user').success(function(response){
-        $scope.user = response;
-        if(response.tmpPassword)  //nel caso ci si trovi nella pagina recuperoPswd
-            $scope.recovering = true;
-    });
     
+    $scope.loadUser = function() {
+      //assegno l'oggetto utente ricevuto dal server a $scope.user  
+        $http.get('/api/profile/get_full_info_user').success(function(response){
+            $scope.user = response;
+            if(response.tmpPassword)  //nel caso ci si trovi nella pagina recuperoPswd
+            $scope.recovering = true; 
+        })
+        .error(function(response){
+            $window.location.href = '/Quizzipedia/home.html';
+            alert("Sessione scaduta");
+        });        
+    };
+    
+   $scope.loadInstitutions = function() {//chiediamo al server un oggetto contenente le affiliazioni dell'utente
+        $scope.institutions = {'Scuola Guida Montello' : 'Studente', 'Liceo Pigafetta' : 'Docente'};
+   };
+
     $scope.changePsw = function(){
         
         $scope.tmp_pswd = null;
-
-    $http.get('/api/profile/get_pswd').success(function(response){
-        $scope.user = response;
-        $scope.recovering = false;
-    });
+        
+        $http.get('/api/profile/get_pswd').success(function(response){
+            $scope.user = response;
+            $scope.recovering = false;
+        });
         
         
     };
     
-    $scope.loadUser = function(){
-        //facciamo una richiesta al server, che ci restituirà l'oggetto utente e successivamente costruiamo l'oggetto utente assegnadolo a $scope.user
-        
-        
-    };
-    
-    $scope.loadAffiliations = function(){
-        //chiediamo al server un oggetto contenente le affiliazioni dell'utente  
-    };    
+    $scope.loadUser();    
+    $scope.loadInstitutions();    
     
     
 }]);

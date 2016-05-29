bd5c0daf4616fd5619c347438ad2be20ee99503a
Bock92 <boccato92@gmail.com>
2016-05-29 15:34:30 +0200

    Aggiunto modulo QuestionManager in core.js

--- public/core.js
+++ public/core.js
@@ -3,3 +3,4 @@ angular.module('userApp', ['userController', 'userService']);
 angular.module('Quizzipedia',[]);
 angular.module('Registration',['Quizzipedia']);
 angular.module('CreateQuestion',['Quizzipedia']);
+angular.module('QuestionManager',['Quizzipedia']);

c6ff07c9a5ca0f454352d6db99eb82afad6bb875
Bock92 <boccato92@gmail.com>
2016-05-29 20:59:21 +0200

    Aggiunte le direttive nel file directiveModify.js

--- public/question_creation/directiveModify.js
+++ public/question_creation/directiveModify.js
@@ -1,9 +1,44 @@
-angular.module('QuestionManager').directive();
+angular.module('QuestionManager').directive('modifyQtf',function(){
+    
+    return {
+      restrict: 'E',
+        templateUrl: './public/question_creation/modifyQTF.html'
+    };
+    
+});
 
-angular.module('QuestionManager').directive();
+angular.module('QuestionManager').directive('modifyQmatch',function(){
+    
+    return {
+        restrict: 'E',
+        templateUrl: './public/question_creation/modifyQMatch.html'
+    };
+    
+});
 
-angular.module('QuestionManager').directive();
+angular.module('QuestionManager').directive('modifyQopen',function(){
+    
+    return {
+        restrict: 'E',
+        templateUrl: './public/question_creation/modifyQOpen.html'
+    };
+    
+});
 
-angular.module('QuestionManager').directive();
+angular.module('QuestionManager').directive('modifyQcompletion',function(){
+    
+    return {
+      restrict: 'E',
+        templateUrl: './public/question_creation/modifyQCompletion.html'
+    };
+    
+});
 
-angular.module('QuestionManager').directive();
+angular.module('QuestionManager').directive('modifyQmultiple',function(){
+
+    return {
+       restrict: 'E',
+        templateUrl: './public/question_creation/modifyQMultiple.html'
+    };
+
+});

8c08049a7922f0d66d8217e143f97b09be0111af
devsoft91 <interbelt@gmail.com>
2016-05-29 19:29:59 +0200

    [server] Aggiunto controllo user notLogged

--- app/controller/profileManager.js
+++ app/controller/profileManager.js
@@ -1,5 +1,5 @@
 var User = require('../model/user.model');
-var Organization = require('../model/organisation.model');
+var Organization = require('../model/organization.model');
 
 exports.getUser = function (req, res) {
 
@@ -24,48 +24,54 @@ exports.getUser = function (req, res) {
 exports.getFullInfoUser = function (req, res) {
 
         var fullUser = {};
-        var email = req.session.user._id;
-        User.findOne({'_id': email}, function (err, user) {
-            if (err) {
-                console.log('error: ' + err);
-                res.redirect('/Quizzipedia/signin');
-            }
-            else {
-                if (user) {  //SE TROVA UN UTENTE NEL DB
-                    fullUser._id = user._id;
-                    fullUser.firstName = user.firstName;
-                    fullUser.lastName = user.lastName;
-                    fullUser.password = user.password;
+        if(req.session.user != undefined){
+            var email = req.session.user._id;
+            User.findOne({'_id': email}, function (err, user) {
+                if (err) {
+                    console.log('error: ' + err);
+                    res.redirect('/Quizzipedia/signin');
+                }
+                else {
+                    if (user) {  //SE TROVA UN UTENTE NEL DB
+                        fullUser._id = user._id;
+                        fullUser.firstName = user.firstName;
+                        fullUser.lastName = user.lastName;
+                        fullUser.password = user.password;
 
-                    Organization.findOne({'name': 'Scuola Guida Montello'}, function (err, org) {
-                        if (err) {
-                          console.log('error: ' + err);
-                        }
-                        else {
-                          if (org) {  //SE TROVA UN UTENTE NEL DB
-                                console.log('org trovato: ' + org._id);
-                                org.users.forEach(function(result,index){
-                                    if(result['user'] === user._id) {
-                                        fullUser.role = result['role'];
-                                        fullUser.state = result['state'];
-                                        res.send(fullUser);
-                                    }
-                                });
-                              }
-                              else {  //SE NON TROVA UN UTENTE NEL DB
-                                console.log('org non trovato');
+                        Organization.findOne({'name': 'Scuola Guida Montello'}, function (err, org) {
+                            if (err) {
+                              console.log('error: ' + err);
                             }
-                          }
-                    });
+                            else {
+                              if (org) {  //SE TROVA UN UTENTE NEL DB
+                                    console.log('org trovato: ' + org._id);
+                                    org.users.forEach(function(result,index){
+                                        if(result['user'] === user._id) {
+                                            fullUser.role = result['role'];
+                                            fullUser.state = result['state'];
+                                            res.send(fullUser);
+                                        }
+                                    });
+                                  }
+                                  else {  //SE NON TROVA UN UTENTE NEL DB
+                                    console.log('org non trovato');
+                                }
+                              }
+                        });
 
-                // fine organization
-                    
+                    // fine organization
+                        
+                    }
+                    else {  //SE NON TROVA UN UTENTE NEL DB
+                        console.log('user non trovato');
+                    }
                 }
-                else {  //SE NON TROVA UN UTENTE NEL DB
-                    console.log('user non trovato');
-                }
-            }
-        });
+            });
+        }
+        else{
+            fullUser.role = null;
+            res.send(fullUser);
+        }
 };
 
 exports.setPassword = function (req, res) {

6a61aae1a662afe64f8157d169e8ae0bcb34d455
BikuM <vassiliki.menarin@gmail.com>
2016-05-30 12:26:43 +0200

    Modifiche utenti e enti

--- public/header/CtrlHeader.js
+++ public/header/CtrlHeader.js
@@ -1,23 +1,31 @@
 angular.module('Quizzipedia').controller('CtrlHeader',['$scope','$http',function($scope,$http){    
     
     $scope.user = null;
+    $scope.institutions = { 'Ancora in nessun ente' : 'Nessun ruolo assegnato' };
+    $scope.currentInstitution = "Nessun ente selezionato";
     $scope.currentHeader = null; 
     
     $scope.loadUser = function() {
       //assegno l'oggetto utente ricevuto dal server a $scope.user  
         $http.get('/api/profile/get_full_info_user').success(function(response){
-            $scope.user = response;
-            
-            })        
-    
+            $scope.user = response;            
+            })    
     };
-    /*}).error(function(response){
-            alert("Errore");
-        });
-        
-    }; */     
     
-   $scope.loadUser();    
+    $scope.loadInstitutions = function() {//chiediamo al server un oggetto contenente le affiliazioni dell'utente
+        $http.get('/api/institution/fetch_user_inst').success(function(response) {
+            $scope.institutions = repsponse;
+        })        
+   };
+    
+    $scope.changeInst = function(name){
+        $http.post('/api/institution/fetch_user_inst', name).success(function(response) {
+            $route.reload();
+        })        
+   };
+    
+   $scope.loadUser();
+   $scope.loadInstitutions();
    
     
 }]);

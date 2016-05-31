eed11e1da248aea82a54bed80367dd0cad321b8b
devsoft91 <interbelt@gmail.com>
2016-05-31 15:32:12 +0200

    [client] Rimosso cartella test test_user

--- public/test_user/CtrlFullUser.js
+++
@@ -1,9 +0,0 @@
-angular.module('usermanager',[]).controller('CtrlFullUser',['$scope','$http',function($scope,$http){
-    
-  $scope.user = {};
-
-  $http.get('/api/profile/get_full_info_user').success(function(response){
-      $scope.user = response;
-  });
-
-}]);

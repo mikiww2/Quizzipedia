a6f2492ca587034331d6c1ed1631116777108132
devsoft91 <interbelt@gmail.com>
2016-05-31 15:33:37 +0200

    [server] Aggiunto funzione fetchNoUserInst()

--- app/controller/institutionManager.js
+++ app/controller/institutionManager.js
@@ -50,6 +50,44 @@ exports.fetchUserInst = function (req, res) {
 		}
 };
 
+exports.fetchNoUserInst = function (req, res) {
+
+		var results = [];
+		if(req.session.user){
+			Organization.find(function (err,orgs){
+				if (err) {
+	            console.log('error: ' + err);
+	            res.redirect('/');
+	      }
+	      else{
+	       	if(orgs){
+	       		for(var i=0;i<orgs.length;i++){
+        			var org = orgs[i];
+        			var found = false;
+        			if(org.director != req.session.user._id){
+        				org.users.forEach(function(result,index){
+                  if(result['user'] === req.session.user._id) {
+                  	found = true;
+                	}
+				        });
+				        if(!found){
+                	results.push({
+                    institution_name: org.name
+                	});
+                }
+				      }
+        		} // fine ciclo for
+        		res.send(results);
+			    }
+			    else{
+       			console.log('error no istitutions found');
+       			res.redirect('/');
+       		}
+       	}	       		
+	    });
+		}
+};
+
 
 
 exports.changeInst = function (req, res) {

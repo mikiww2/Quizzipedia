363c79625b59cab62a5d3bc4fd7ebe0eedec9940
BikuM <vassiliki.menarin@gmail.com>
2016-06-01 00:57:27 +0200

    Creati controller per invio richieste

--- public/core.js
+++ public/core.js
@@ -3,6 +3,6 @@ angular.module('userApp', ['userController', 'userService']);
 angular.module('Quizzipedia',[]);
 angular.module('Registration',['Quizzipedia']);
 angular.module('CreateQuestion',['Quizzipedia']);
-angular.module('QuestionManager',['Quizzipedia','CreateQuestion']);
+angular.module('QuestionManager',['Quizzipedia', 'CreateQuestion']);
 angular.module('ProfileManager', ['Quizzipedia']);
 angular.module('RequestsManager', ['Quizzipedia']);

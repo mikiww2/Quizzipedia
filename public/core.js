65ed7d23375d3c68afc35dfb9b15890b636caad1
Bock92 <boccato92@gmail.com>
2016-06-01 22:14:11 +0200

    aggiunta una dipendenza al module CreateQuestion

--- public/core.js
+++ public/core.js
@@ -2,7 +2,7 @@ angular.module('userApp', ['userController', 'userService']);
 
 angular.module('Quizzipedia',[]);
 angular.module('Registration',['Quizzipedia']);
-angular.module('CreateQuestion',['Quizzipedia']);
+angular.module('CreateQuestion',['Quizzipedia','ngFileUpload']);
 angular.module('QuestionManager',['Quizzipedia', 'CreateQuestion']);
 angular.module('ProfileManager', ['Quizzipedia']);
 angular.module('RequestsManager', ['Quizzipedia']);

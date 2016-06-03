efb6d1e670c9bb78816f40ebaaccdc3e735f4968
Bock92 <boccato92@gmail.com>
2016-06-03 18:15:20 +0200

    create due nuove classi per gestire la creazione delle domande a risposta aperta

---
+++ public/services/AnswerMultipleChoice.js
@@ -0,0 +1,52 @@
+angular.module('CreateQuestion').factory('AnswerMultipleChoice',['Attachment',function(Attachment){
+    
+    
+    function AnswerMultipleChoice(){
+        this.textAnswer = null; //se è null vuol dire che è una risposta che ha solo l'allegato
+        this.isTrue = false;
+        this.attachment = null; //se è null vuol dire che è una risposta testuale
+                                //tipo di attachemnt: Attachment
+        
+    };
+    
+    
+    AnswerMultipleChoice.prototype.getTextAnswer = function(){
+      return this.textAnswer;  
+    };
+    
+    AnswerMultipleChoice.prototype.isTrue = function(){
+      return this.isTrue;  
+    };
+    
+    AnswerMultipleChoice.prototype.getAttachment = function(){
+      return this.attachment;  
+    };
+    
+    AnswerMultipleChoice.prototype.setEmptyAttachment = function(){
+        this.attachment = new Attachment();
+    };
+    
+    AnswerMultipleChoice.prototype.setAttachmentType = function(type){
+        this.attachment.setType(type);
+    };
+    
+    AnswerMultipleChoice.prototype.setAttachmentPath = function(path){
+        this.attachment.setPath(path);  
+    };
+    
+    AnswerMultipleChoice.prototype.setAttachmentCoordinateX = function(x_coordinate){
+        this.attachment.setX(x_coordinate);  
+    };
+    
+    
+    AnswerMultipleChoice.prototype.setAttachmentCoordinateY = function(y_coordinate){
+        this.attachment.setY(y_coordinate);  
+    };
+    
+    
+    
+    
+    
+    return AnswerMultipleChoice;
+    
+}]);

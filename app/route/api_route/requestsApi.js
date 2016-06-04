bf40e65e8f46d0a57846de541f74be57b2cac70f
devsoft91 <interbelt@gmail.com>
2016-06-04 11:53:17 +0200

    [server] Aggiunto API /api/requests/add_inst_role_request

--- app/route/api_route/requestsApi.js
+++ app/route/api_route/requestsApi.js
@@ -1,8 +1,10 @@
-var authenticationM = require('../../controller/requestsManager');
+var requestsM = require('../../controller/requestsManager');
 
 module.exports = function (app) {
 
-  
+  app.post('/api/requests/add_inst_role_request', function (req, res, next) {
+    requestsM.addInstitutionRoleRequest(req, res);
+  });
 
 }; 
  

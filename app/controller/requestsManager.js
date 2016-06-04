7b8c0a22e8419c32c22e0a41b9da8761cd37d726
devsoft91 <interbelt@gmail.com>
2016-06-04 11:51:59 +0200

    [server] Aggiunto addInstitutionRoleRequest()

--- app/controller/requestsManager.js
+++ app/controller/requestsManager.js
@@ -1,1 +1,47 @@
- 
+var Organization = require('../model/organization.model');
+
+exports.addInstitutionRoleRequest = function (req, res) {
+
+		if(req.session.user){
+			Organization.findOne({ 'name': req.body.institution }, function (err,org){
+				if (err) {
+	            console.log('error: ' + err);
+	            res.redirect('/');
+	      }
+	      else{
+	       	if(org){
+	       		console.log('sto cercando nell\'organizzazione ' + req.body.institution);
+	       			var found = false;
+	       			org.users.forEach(function(result,index){
+                if(result['user'] === req.session.user._id)
+                  found = true;
+		        	});
+		        	if(!found){
+		        		org.users.push({
+		        			user : req.session.user._id
+		        			,role : req.body.role
+		        			,message : req.body.message
+		        			,state : 'requested'
+		        		});
+		        		org.save( function (err) {
+                    if (err) {
+                        console.log('errore nell\'inserimento della richiesta di ruolo: ' + err);
+                        res.send('/');
+                    }
+                    else {
+                        console.log('richiesta di ruolo inserita correttamente');
+                        res.send('/');
+                    }
+                });
+		        	}
+		        	else{
+		        		console.log('utente già presente');
+		        		res.send('/');
+		        	}
+
+		        }
+		            
+       	}	       		
+	    });
+		}
+};

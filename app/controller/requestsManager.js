9945cd25af1105989fd06f3784e4714b1628ffee
devsoft91 <interbelt@gmail.com>
2016-06-04 16:45:00 +0200

    [server] Aggiunto viewRoleRequests()

--- app/controller/requestsManager.js
+++ app/controller/requestsManager.js
@@ -1,4 +1,53 @@
 var Organization = require('../model/organization.model');
+var User = require('../model/user.model');
+
+exports.viewRoleRequests = function (req, res) {
+
+		var newuser = {};
+		var userlist = [];
+
+		if(req.session.user && req.session.user.role === 'director'){
+			Organization.findOne({ 'name': req.session.user.institution }, function (err,org){
+				if (err) {
+	            console.log('error: ' + err);
+	            res.redirect('/');
+	      }
+	      else{
+	       	if(org){
+	       		console.log('sto cercando nell\'organizzazione ' + req.session.user.institution);
+       			org.users.forEach(function(result,index){
+              if(result['state'] === 'requested'){
+              	User.findOne({ '_id': result['user']}, function (err,user){  //err not handled
+              		if(user){
+              			newuser._id = user._id;
+	              		newuser.firstName = user.firstName;
+	              		newuser.lastName = user.lastName;
+	              		userlist.push(newuser);
+	              		console.log(newuser);
+	              		newuser = { };
+              		}
+              	});
+              }
+
+	        	});
+	        	setTimeout(function(){
+	        		console.log('userlist');
+							res.send(userlist);
+	        	}, 500);
+
+       		}
+       		else{
+       			console.log('Organizzazione non trovata');
+       			res.redirect('/');
+       		}
+		            
+       	}
+
+	    });
+		}
+}
+
+
 
 exports.addInstitutionRoleRequest = function (req, res) {
 

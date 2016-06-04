cec1845f62fba4e281947a3843d2b41f35ae4fb0
devsoft91 <interbelt@gmail.com>
2016-06-04 11:46:49 +0200

    [client] Perfezionato conformità dati

--- public/requests/requestRole.js
+++ public/requests/requestRole.js
@@ -1,4 +1,4 @@
-angular.module('RequestsManager').directive('requestRole', function(){
+angular.module('RequestsManager').directive('requestRole',['$window', function($window){
     
     return {
         restrict: 'E',
@@ -17,7 +17,7 @@ angular.module('RequestsManager').directive('requestRole', function(){
                     $scope.user = response;
                 })                
                 .error(function(response){
-                    $window.location.href = '/Quizzipedia/home.html';
+                    $window.location.href = '/Quizzipedia/home';
                     alert("Sessione scaduta");
                 });        
             };
@@ -25,18 +25,23 @@ angular.module('RequestsManager').directive('requestRole', function(){
             $scope.loadInst = function () {
                 $http.get('/api/institution/fetch_no_user_inst').success(function(response) {
                 $scope.institutions = response;
-                })
+                });
             };            
             
             
-            $scope.sendRoleRequest = function () {                
+            $scope.sendRoleRequest = function () {
+                if($scope.role == 'Studente')
+                    $scope.role = 'student';
+                else $scope.role = 'teacher';
                 var request = { user : $scope.user._id, institution : $scope.institution, role : $scope.role, message: $scope.message };
                 
-                //$http.post('Definire api', request);
+                $http.post('/api/requests/add_inst_role_request', request).success(function(response) {
+                    $window.location.href = response;
+                });
             };
             
             $scope.loadUser();
             $scope.loadInst();
        }
     };
-});
+}]);

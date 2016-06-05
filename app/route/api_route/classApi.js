4e334d45d9712e27a9c22e46d0209a559ea89528
devsoft91 <interbelt@gmail.com>
2016-06-05 19:04:57 +0200

    [server] Corretto path API fetch_no_user_class

--- app/route/api_route/classApi.js
+++ app/route/api_route/classApi.js
@@ -2,7 +2,7 @@ var classM = require('../../controller/classManager');
 
 module.exports = function (app) {
 
-  app.get('/api/institution/fetch_no_user_class',function (req, res, next) {
+  app.get('/api/class/fetch_no_user_class',function (req, res, next) {
     classM.fetchNoUserClass(req, res);
   });
 

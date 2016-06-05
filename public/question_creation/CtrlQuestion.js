20c74417860a2637e5f19cc14e5b5ba650b19308
Bock92 <boccato92@gmail.com>
2016-06-05 11:09:24 +0200

    aggiunto html e funzionalità al controller per upload di file

--- public/question_creation/CtrlQuestion.js
+++ public/question_creation/CtrlQuestion.js
@@ -1,4 +1,4 @@
-angular.module('CreateQuestion').controller('CtrlQuestion',['$scope','$http','TrueFalseQ','ShortAnswerQ','MultipleChoiceQ','AnswerMultipleChoice','Upload','$timeout', function($scope, $http, TrueFalseQ,ShortAnswerQ,MultipleChoiceQ,AnswerMultipleChoice,Upload,$timeout){ //dipendenze verso tutti i tipi di domande e Topics
+angular.module('CreateQuestion').controller('CtrlQuestion',['$scope','$http','TrueFalseQ','ShortAnswerQ','MultipleChoiceQ','AnswerMultipleChoice','Attachment','Upload','$window','$timeout', function($scope, $http, TrueFalseQ,ShortAnswerQ,MultipleChoiceQ,AnswerMultipleChoice,Attachment,Upload,$window,$timeout){ //dipendenze verso tutti i tipi di domande e Topics
     
     $scope.topics = []; //inizializzato dal server
     
@@ -34,82 +34,31 @@ angular.module('CreateQuestion').controller('CtrlQuestion',['$scope','$http','Tr
       
         question: new MultipleChoiceQ(),
         size: 0,
-        create: function(index){
-            this.question.answers.push("");
-            this.question.answerAttachment.push(null);
-            this.question.correctAnswer.push(false);
+        create: function(index){ //per i textarea
+            var x = new AnswerMultipleChoice();
+            this.question.addAnswer(x);
+            this.question.arrayAnswer[index].textAnswer = "";
             this.size = this.size +1;
         },
         remove: function(index){
-            if(index>= 0 && index < this.question.answers.length){
-                this.question.answers.splice(index,1);
-                this.question.answerAttachment.splice(index,1);
-                this.question.correctAnswer.splice(index,1);
-                this.size = this.size - 1;
-            }
+            this.question.removeAnswer(index);
+            this.size = this.size -1;
         },
         preUpload: function(index){
-            this.question.answers.push(null);
-            this.question.answerAttachment.push("");
-            this.question.correctAnswer.push(false);
+            var x = new AnswerMultipleChoice();
+            this.question.addAnswer(x);
+            this.question.setEmptyAttachment(index);
             this.size = this.size + 1;
         }
-    };
-    
-    
-    $scope.uploadPic = function(file){
-      
-        file.upload = Upload.upload({
-            url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-            data: {file: file},
-        });
         
-        file.upload.then(function(response){
-            $timeout(function(){
-                file.result = response.data;
-            });
-        }, function(response){
-            if (response.status > 0){
-                $scope.errorMsg = response.status + ': ' + response.data;
-            }
-        }, function(evt){
-            file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-        });
+        
         
     };
     
+   
+   
     
-    /*$rootScope.callWindowMethod = function(methodName, parameter){
-        if(typeof window[methodName] !== 'function'){
-            console.log('Internal Error');
-            return;
-        }
-        return window[methodName](parameter);
-    };
-    */
-    
-    
-    /*$scope.MyMultipleChoiceQ = {
-        correctAnswer: [],
-        wrongAnswer: [],
-        insertAnswer: function(text, check){//name:String della risposta  check:bool memorizza se corretto (se true faccio il push in correctAnswer)
-            if(check){
-                $scope.multipleChoiceSection.correctAnswer.push(name);
-            }
-            else{
-                $scope.multipleChoiceSection.wrongAnswer.push(name);
-            }
-        },
-        removeAnswer: function(text, check){ //check == true devo cerco la risposta dentro a correctAnswer altrimenti wrongAnswer
-            
-        },
-        reset: function(){
-            this.correctAnswer = [];
-            this.wrongAnswer = [];
-        }
-        
-    };*/
-    
+       
     $scope.MyCompletionQ ={
       
         text: null,
@@ -213,19 +162,89 @@ angular.module('CreateQuestion').controller('CtrlQuestion',['$scope','$http','Tr
         
     };
     
+    $scope.localfiles = [];
+    
+    /*$scope.onFileSelect = function($files){
+        for(var i = 0; i< $files.length; i++){
+            $scope.localfiles.push($files[i]);
+        }
+    };*/
+
+    
+    
+    $scope.uploadFiles = function(files){
+        
+        $scope.files = files;
+        if (files && files.length){
+            Upload.upload({
+                url:'/api/question/test',
+                data: {files: files}
+            }).then(function(response){
+                $timeout(function(){
+                    $scope.result = response;
+                });
+            }, function(evt){
+                $scope.progress = Math.min(100,parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }
+        
+        
+    };
+    
+    
+    
     $scope.save = function(question){
       //salvo la domanda creata        
         
+        var json = {
+          domanda: question,
+            file: $scope.localfiles
+        };
+        
 
-        $http.post('/api/question/save',question).success(function(response){
+       $http.post('/api/question/test',json).success(function(response){
             
-            $http.get('/api/question/fetch').success(function(response){
-                $scope.domande = response;
-            });
+            //$http.get('/api/question/fetch').success(function(response){
+              //  $scope.domande = response;
+            //});
         
-        
+            alert(response.toJson);
         });
     };
+    
+    
+     //GESTIONE UPLOAD FILE
+    
+   /* $scope.submit = function(){
+      
+        if($scope.upload_form.file.$valid && $scope.file){
+            $scope.upload($scope.file);
+        }
+        
+    };*/
+    
+    /*$scope.upload = function(file){
+        Upload.upload({
+            url: 'http://localhost:8080/upload', //API
+            data: {file: file},
+            
+        }).then(function(response){
+            if(response.data.error_code === 0){
+                $window.alert('Success' + response.config.data.file.name + 'uploaded. Response: ');
+            }
+            else {
+                $window.alert('an error accured');
+            }
+        },function(response){
+            console.log('Error status: '+ response.status);
+            $window.alert('Error status: '+ response.status);
+        },function(evt){
+            console.log(evt);
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: '+ progressPercentage + '%'+ evt.config.data.file.name);
+            $scope.progress = 'progress: '+ progressPercentage + '%';
+        });
+    };*/
     
     
     

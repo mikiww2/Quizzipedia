9919d98e053f137f72250a913adfd01f4ba1f2a8
Bock92 <boccato92@gmail.com>
2016-06-07 13:19:47 +0200

    aggiunto il metodo per salvare gli argomenti nel server

--- public/topics/CtrlTopics.js
+++ public/topics/CtrlTopics.js
@@ -1,24 +1,31 @@
 angular.module('TopicsManager').controller('CtrlTopics',['Topics','$scope','$http',function(Topics,$scope,$http){
     
-    $scope.topicsList = new Topics();
+    
+     $scope.loadTopics = function(){
+        
+        var x = new Topics();
+        return x;
+      
+        //faccio una richiesta al server che mi ritorna un array di topics e assegno l'oggetto a listTopics
+    };
+    
+    
+    
+    
+    $scope.topicsList = $scope.loadTopics();
     
     $scope.eraseTopic = null; //index topic
     
     $scope.topicName = null; //variabile che si ricorda il nome del topic inserito nella form
     
-    $scope.loadTopics = function(){
-      
-        //faccio una richiesta al server che mi ritorna un array di topics e assegno l'oggetto a listTopics
-    };
+   
     
      $scope.addTopic = function(name){
          //inseriamo il nuovo topic dentro a listTopics e invio una richiesta al server
          
          $scope.topicsList.addTopic(name);
-         /*
-         $http.post('',name).success(function(response){
-             
-         });*/
+         
+         $http.post('/api/topic/save',name);
      };
     
      $scope.removeTopic = function(index){

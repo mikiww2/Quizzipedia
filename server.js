610acb8684d319ba9bfc1c6425c985d998007737
Bock92 <boccato92@gmail.com>
2016-06-08 10:22:30 +0200

    inserita inclusione della topicApi.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ require('./app/route/api_route/requestsApi.js')(app);  // RESTful requests
 require('./app/route/api_route/searchApi.js')(app);  // RESTful requests
 require('./app/route/api_route/institutionApi.js')(app);  // RESTful requests
 require('./app/route/api_route/uploadApi.js')(app);  // RESTful requests
+require('./app/route/api_route/topicApi.js')(app);  // RESTful requests
 
 require('./app/route/route.js')(app);  // PAGES requests
 

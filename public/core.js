822f0fa83c19d7ab8f8bc81b03cc0f0e5b78f0f1
Bock92 <boccato92@gmail.com>
2016-06-08 23:43:22 +0200

    aggiunta una dipendenza al module('QuestionManager')

--- public/core.js
+++ public/core.js
@@ -3,7 +3,7 @@ angular.module('userApp', ['userController', 'userService']);
 angular.module('Quizzipedia',[]);
 angular.module('Registration',['Quizzipedia']);
 angular.module('CreateQuestion',['Quizzipedia','ngFileUpload']);
-angular.module('QuestionManager',['Quizzipedia', 'CreateQuestion']);
+angular.module('QuestionManager',['Quizzipedia', 'CreateQuestion', 'ngFileUpload']);
 angular.module('ProfileManager', ['Quizzipedia']);
 angular.module('RequestsManager', ['Quizzipedia']);
 angular.module('InstClassManager', ['Quizzipedia']);

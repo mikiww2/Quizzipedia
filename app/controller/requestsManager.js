c6f76976113b37554b7d80518f5766c6e013e6d0
devsoft91 <interbelt@gmail.com>
2016-06-08 23:37:17 +0200

    [server] Aggiunta funzione viewClassRequests()

--- app/controller/requestsManager.js
+++ app/controller/requestsManager.js
@@ -16,7 +16,7 @@ exports.viewRoleRequests = function (req, res) {
 	       	if(org){
 	       		console.log('sto cercando nell\'organizzazione ' + req.session.user.institution);
        			org.users.forEach(function(result,index){
-              if(result['state'] === 'requested'){
+              if(result['state'] == 'requested'){
               	User.findOne({ '_id': result['user']}, function (err,user){  //err not handled
               		if(user){
               			newuser._id = user._id;
@@ -51,6 +51,89 @@ exports.viewRoleRequests = function (req, res) {
 		else res.redirect('/');
 }
 
+exports.viewClassRequests = function (req, res) {
+
+		var response = [];
+
+		if(req.session.user && req.session.user.role == 'director'){
+			Organization.findOne({ 'name': req.session.user.institution }, function (err,org){
+				if (err) {
+	            console.log('error: ' + err);
+	            res.redirect('/');
+	      }
+	      else{
+	       	if(org){
+	       		console.log('sto cercando nell\'organizzazione ' + req.session.user.institution);
+       			for(var i=0;i<org.users.length;i++){
+       				if(org.users[i].role == 'teacher')
+       					for(var j=0;j<org.users[i].classes.length;j++){
+	       					if(org.users[i].classes[j].state == 'requested')
+	       						for(var k=0;k<org.classes.length;k++){
+       								if(org.classes[k]._id == org.users[i].classes[j].class)
+       									response.push({
+       										user: org.users[i].user,
+       										class: org.users[i].classes[j].class,
+       										description: org.classes[k].description,
+       										name: org.classes[k].name,
+       										academicYear: org.classes[k].academicYear
+       									});
+       							}	
+	       				}
+       			}
+       			res.send(response);
+       		}
+       		else{
+       			console.log('Organizzazione non trovata');
+       			res.redirect('/');
+       		}
+		    }
+	    });
+		}
+		else{
+			if(req.session.user && req.session.user.role == 'teacher'){
+				Organization.findOne({ 'name': req.session.user.institution }, function (err,org){
+					if (err) {
+		            console.log('error: ' + err);
+		            res.redirect('/');
+		      }
+		      else{
+		       	if(org){
+		       		console.log('sto cercando nell\'organizzazione ' + req.session.user.institution);
+	       			for(var i=0;i<org.users.length;i++){
+	       				if(org.users[i].user == req.session.user._id)
+	       					for(var j=0;j<org.users[i].classes.length;j++){
+		       					if(org.users[i].classes[j].state == 'allowed')
+		       						for(var k=0;k<org.users.length;k++){
+	       								if(org.users[k].state == 'student')
+	       									for(var h=0;h<org.users[k].classes.length;h++){
+	       										if(org.users[k].classes[h].state == 'requested')
+	       											for(var w=0;w<org.classes.length;w++){
+	       												if(org.classes[w]._id == org.users[k].classes[h].class)
+	       													response.push({
+					       										user: org.users[k].user,
+					       										class: org.users[k].classes[h].class,
+					       										description: org.classes[w].description,
+					       										name: org.classes[w].name,
+					       										academicYear: org.classes[w].academicYear
+					       									});
+	       											}
+	       									}
+	       							}	
+		       				}
+	       			}
+	       			res.send(response);
+	       		}
+	       		else{
+	       			console.log('Organizzazione non trovata');
+	       			res.redirect('/');
+	       		}
+			    }
+		    });
+			}
+			else res.redirect('/');
+		}
+}
+
 
 exports.addInstitutionRoleRequest = function (req, res) {
 
@@ -65,7 +148,7 @@ exports.addInstitutionRoleRequest = function (req, res) {
 	       		console.log('sto cercando nell\'organizzazione ' + req.body.institution);
 	       			var found = false;
 	       			org.users.forEach(function(result,index){
-                if(result['user'] === req.session.user._id)
+                if(result['user'] == req.session.user._id)
                   found = true;
 		        	});
 		        	if(!found){
@@ -109,10 +192,10 @@ exports.addClassInsertRequest = function (req, res) {
 	      else{
 	       	if(org){
 	       			org.users.forEach(function(result,index){
-                if(result['user'] === req.session.user._id){
+                if(result['user'] == req.session.user._id){
                 	var classs = {
                 		state: 'requested',
-                		class: req.body.name
+                		class: req.body._id
                 	};
                 	org.users[index].classes.push(classs);
                 }

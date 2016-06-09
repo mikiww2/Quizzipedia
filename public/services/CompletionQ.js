ccdab44daedd027e313baf5fd3e14b6269d79836
ftess <filippo90t@gmail.com>
2016-06-09 15:33:48 +0200

    Inserito commento di spiegazione per donbock

--- public/services/CompletionQ.js
+++ public/services/CompletionQ.js
@@ -6,8 +6,28 @@ angular.module().factory('CompletionQ',['GenericQuestion',function(){
         this.text = null; //String
         this.suggestions = []; //String[]
     };
-    
-    
+    /* il testo della domanda deve essere un array con questi json dentro 
+    {'type': 'txt/id', 'value': 'blocco di testo della domanda se è tipo txt/ id della risposta se tipo id'}
+
+    la risposta è un array con dentro json fatti cosi
+    {'text': 'possibile risposta', 'id': 'id che indica il buco dove sarebbe corretta'}
+
+     ti allego un esempio per capirti
+       title: 
+   [ { type: 'txt', value: 'napoleone è nato nel ' },
+     { type: 'id', value: '1' },
+     { type: 'txt', value: '. Nel ' },
+     { type: 'id', value: '2' },
+     { type: 'txt', value: 'è stato esiliato nell\' isola d\'' },
+     { type: 'id', value: '3' } ],
+  ans: 
+   [ { text: '1800', id: '1' },
+     { text: '1850', id: '2' },
+     { attachment: [Object], id: '3' } ] }
+
+    per il type di question e le altre informazioni rimane tutto uguale come per le altre tipologie di domanda
+
+    */
     
     CompletionQ.prototype.setText = function(newText){
       

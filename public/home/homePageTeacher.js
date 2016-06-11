0473e8e68ace1b1c95bc694cc23a1b78b7ddc04b
BikuM <vassiliki.menarin@gmail.com>
2016-06-11 19:42:32 +0200

    [client] Sistemate classi nella homepage del docente

--- public/home/homePageTeacher.js
+++ public/home/homePageTeacher.js
@@ -1,10 +1,19 @@
-angular.module('Quizzipedia').directive('homePageTeacher',function(){
-    
+angular.module('Quizzipedia').directive('homePageTeacher', function() {
     return {
-      
         restrict: 'E',
-        templateUrl: './public/home/homepageTeacher.html'
-    };
-    
+        templateUrl: './public/home/homepageTeacher.html',
+        controller: function($scope, $http) {
+            
+            $scope.classes = [];
+            
+            $scope.loadClasses = function () {                
+                $http.get('/api/class/fetch_teacher_classes_list').success (function (response) {
+                    $scope.classes = response;
+                });
+            }
+            
+            $scope.loadClasses();
+        }
+    };    
     
 });

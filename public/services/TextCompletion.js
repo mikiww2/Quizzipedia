0cf796ce0352415add3b91e52ad419d8110b38f2
Bock92 <boccato92@gmail.com>
2016-06-11 15:59:24 +0200

    creata nuova factory che rappresenta il testo della domanda a completamento testo

---
+++ public/services/TextCompletion.js
@@ -0,0 +1,32 @@
+angular.module('CreateQuestion').factory('TextCompletion',[function(){
+    
+    function TextCompletion(){
+      
+        this.type = null; //'txt/id'
+        this.value = null; //testo domanda --> type = txt 
+                           //id buco --> type = id
+        
+    };
+    
+    TextCompletion.prototype.getType = function(){
+      return this.type;  
+    };
+    
+    TextCompletion.prototype.getValue = function(){
+      return this.value;  
+    };
+    
+    TextCompletion.prototype.setType = function(type){
+      this.type = type;  
+    };
+    
+    TextCompletion.prototype.setValue = function(value){
+        this.value = value;
+    };
+    
+    
+    
+    return TextCompletion;
+    
+    
+}]);

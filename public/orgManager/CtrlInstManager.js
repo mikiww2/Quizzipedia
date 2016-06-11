a727aa4c1c0db61b177407278037cf09fc708a53
BikuM <vassiliki.menarin@gmail.com>
2016-06-11 17:12:43 +0200

    [client] Sistemata pagina instList

--- public/orgManager/CtrlInstManager.js
+++ public/orgManager/CtrlInstManager.js
@@ -1,44 +1,14 @@
-angular.module('InstClassManager').controller('CtrlInstManager',['Class', '$scope', '$http', function (Class, $scope, $http){
+angular.module('InstClassManager').controller('CtrlInstManager',['$scope', '$http', function ($scope, $http){
     
-    $scope.classes = [];
-    $scope.myClass = new Class();
-    $scope.index = null; 
+    $scope.institutions = [];    
     
-    $scope.loadClasses = function() {
-        $http.get('/api/class/fetch_inst_classes').success(function(response) {
-            $scope.classes = response;
+    $scope.loadInstitutions = function() {
+        $http.get('/api/institution/fetch_all_inst_infos ').success(function(response) {
+            $scope.institutions = response;
         });
     };
     
-    $scope.createClass = function() {
-        var request = { description : $scope.myClass.getDescription(), name : $scope.myClass.getName(), year : $scope.myClass.getAcademicYear()};
-        $http.post('/api/class/create_class', request); 
-        $scope.classes.push(request);
-        $scope.myClass = new Class();       
-    };
-    
-    $scope.delete = function() {
-        var request = {_id : $scope.classes[$scope.index]._id};
-        $http.post('/api/class/remove_class', request);
-        $scope.classes.splice($scope.index, 1);
-        $scope.myClass = new Class();
-        $scope.index = null;        
-    };
-    
-    $scope.edit = function (){
-        var request = {_id : $scope.classes[$scope.index]._id, description : $scope.myClass.getDescription()};
-        $http.post('/api/class/update_class', request);
-        $scope.classes[$scope.index].description = $scope.myClass.getDescription();
-        $scope.myClass = new Class();
-        $scope.class_id = null;
-    };
-    
-    $scope.selectClass = function (indexOfClass) {
-        $scope.myClass.edit($scope.classes[indexOfClass].description, $scope.classes[indexOfClass].name, $scope.classes[indexOfClass].academicYear);    
-        $scope.index = indexOfClass;
-    };
-    
-    $scope.loadClasses();
+    $scope.loadInstitutions();
     
 }]);
     

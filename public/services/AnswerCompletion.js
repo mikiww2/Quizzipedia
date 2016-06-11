dcaf34d6fd3f56beac55207970f0844d2b53939d
Bock92 <boccato92@gmail.com>
2016-06-11 15:58:29 +0200

    creata nuova factory che rappresenta le risposte della domanda a completamento testo

---
+++ public/services/AnswerCompletion.js
@@ -0,0 +1,32 @@
+angular.module('CreateQuestion').factory('AnswerCompletion',[function(){
+    
+    
+    function AnswerCompletion(){
+      
+        this.text = null;
+        this.id = null;
+        
+    };
+    
+    
+    
+    AnswerCompletion.prototype.getText = function(){
+       return this.text;  
+    };
+    
+    AnswerCompletion.prototype.getId = function(){
+        return this.id;
+    };
+    
+    AnswerCompletion.prototype.setText = function(text){
+        this.text = text;  
+    };
+    
+    AnswerCompletion.prototype.setId = function(id){
+        this.id = id;  
+    };
+    
+    
+    return AnswerCompletion;
+    
+}]);

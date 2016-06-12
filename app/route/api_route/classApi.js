74afb6b5fd9de1229ae20b878f6aed64dd0b1a1a
devsoft91 <interbelt@gmail.com>
2016-06-12 19:28:16 +0200

    [server] Cambiato nome API e adattato fx anche al director

--- app/route/api_route/classApi.js
+++ app/route/api_route/classApi.js
@@ -15,25 +15,25 @@ var classM = require('../../controller/classManager');
 
 module.exports = function (app) {
 
-  app.get('/api/class/fetch_teacher_classes_list',function (req, res, next) {
-    classM.fetchTeacherClassesList(req, res);
-  });
-
-  app.get('/api/class/fetch_teacher_classes_details',function (req, res, next) {
-    classM.fetchTeacherClassesDetails(req, res);
-  });
-
-  app.post('/api/class/fetch_class_members',function (req, res, next) {
-    classM.fetchClassMembers(req, res);
-  });
-
-	app.get('/api/class/fetch_inst_classes',function (req, res, next) {
+  app.get('/api/class/fetch_inst_classes',function (req, res, next) {
     classM.fetchInstClasses(req, res);
   });
 
   app.get('/api/class/fetch_no_user_class',function (req, res, next) {
     classM.fetchNoUserClass(req, res);
   });
+
+  app.get('/api/class/fetch_classes_list',function (req, res, next) { //ritorna nome e id delle classi
+    classM.fetchClassesList(req, res);                                //in cui si trova il docente
+  });                                                                 //o tutte se è responsabile
+
+  app.get('/api/class/fetch_classes_details',function (req, res, next) { //come la precedente ma in piu
+    classM.fetchClassesDetails(req, res);                                //ritorna anche numero di
+  });                                                                    //docenti e studenti 
+
+  app.post('/api/class/fetch_class_members',function (req, res, next) {
+    classM.fetchClassMembers(req, res);
+  });  
 
   app.post('/api/class/remove_from_class',function (req, res, next) {
     classM.removeFromClass(req, res);

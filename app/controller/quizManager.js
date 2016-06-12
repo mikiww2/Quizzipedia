18260920ef33cd4efb8a545d6bb2b19a54508caf
devsoft91 <interbelt@gmail.com>
2016-06-13 01:49:22 +0200

    [server] Aggiunti controller ed API quiz

--- app/controller/quizManager.js
+++ app/controller/quizManager.js
@@ -1,1 +1,82 @@
- 
+/*
+ * Nome del file: quizManager.js
+ * Percorso: app/controller/quizManager.js
+ * Autore: Vault-Tech
+ * Data creazione:
+ * E-mail: vaulttech.swe@gmail.com
+ *
+ *  Controller per il salvataggio, recupero e rimozione di quiz 
+ *
+ * * Diario delle modifiche:
+ *
+ */
+
+var Quiz = require('../model/quiz.model');
+
+exports.fetchUserQuiz = function (req,res) { //quiz creati dal docente loggato nel relativo ente
+
+	var results = [];
+	if(req.session.user && req.session.user.role == 'teacher'){
+		Quiz.find(function (err,quiz){
+			if (err) {
+            console.log('error: ' + err);
+            res.redirect('/');
+      }
+      else{
+       	if(quiz){
+       		for(var i=0;i<quiz.length;i++){
+       			if(quiz[i].author == req.session.user._id && quiz[i].institution == req.session.user.institution){
+       				results.push(quiz[i]);
+       			}
+       		}
+       		res.send(results);
+       	}
+       	else console.log('Nessun quiz trovato');
+
+			}
+
+		});
+	}
+	else res.redirect('/');
+
+}
+
+exports.fetchAllQuiz = function (req,res) { //tutti i quiz dell'ente, non so se serva
+
+}
+
+exports.fetchQuizNumber = function (req, res, next) {
+
+    if(req.session.user){ //&& req.session.user.role == 'teacher' da aggiungere dopo
+
+        Quiz.find({ 'institution': req.session.user.institution }, function (err, quiz) {
+            if (err) {
+                console.log('error: ' + err);
+                res.redirect('/');
+            }
+            else{
+                if(quiz){
+                    res.send({ number: quiz.length });
+                }
+                else{
+                    console.log('Nessuna domanda trovata');
+                    res.send({ number: 0 });
+                }
+            }
+        });
+
+    }
+    else res.redirect('/');
+};
+
+exports.fetchPublicQuiz = function (req,res) { //quiz dell'ente pubblici
+
+}
+
+exports.search = function (req,res) { //ricerca quiz
+
+}
+
+exports.save = function (req,res) { //salvataggio quiz
+
+}
--- app/route/api_route/quizApi.js
+++ app/route/api_route/quizApi.js
@@ -11,11 +11,33 @@
  *
  */
 
-var authenticationM = require('../../controller/quizManager');
+var quizM = require('../../controller/quizManager');
 
 module.exports = function (app) {
 
-  
+  app.get('/api/quiz/fetch_user_quiz',function (req, res, next) { //quiz creati dal docente loggato
+    quizM.fetchUserQuiz(req, res);
+  });
+
+  app.get('/api/quiz/fetch_all_quiz',function (req, res, next) { //tutti i quiz dell'ente, non so se serva
+    quizM.fetchAllQuiz(req, res);
+  });
+
+  app.get('/api/quiz/fetch_quiz_number',function (req, res, next) { //numero quiz nell'istituto
+    quizM.fetchQuizNumber(req, res);
+  });
+
+  app.get('/api/quiz/fetch_public_quiz',function (req, res, next) { //quiz dell'ente pubblici
+    quizM.fetchPublicQuiz(req, res);
+  });
+
+  app.post('/api/quiz/search',function (req, res, next) { //ricerca quiz
+    quizM.search(req, res);
+  });
+
+  app.post('/api/quiz/save',function (req, res, next) { //salvataggio quiz
+    quizM.save(req, res);
+  });
 
 }; 
  

1bbb4faa43d240bc4c28926e1ce44dbab9d07571
devsoft91 <interbelt@gmail.com>
2016-06-12 22:39:15 +0200

    [server] Aggiunta API /api/institution/fetch_number_teachers

--- app/route/api_route/institutionApi.js
+++ app/route/api_route/institutionApi.js
@@ -15,20 +15,24 @@ var institutionM = require('../../controller/institutionManager');
 
 module.exports = function (app) {
 
-  app.get('/api/institution/fetch_all_inst_infos',function (req, res, next) {
+  app.get('/api/institution/fetch_all_inst_infos',function (req, res, next) { //informazioni generali enti anche x utente non loggato
     institutionM.fetchAllInstInfos(req, res);
   });
 
-  app.get('/api/institution/fetch_user_inst',function (req, res, next) {
+  app.get('/api/institution/fetch_user_inst',function (req, res, next) { //enti e ruoli nei quali è iscritto l'utente
     institutionM.fetchUserInst(req, res);
   });
 
-  app.get('/api/institution/fetch_no_user_inst',function (req, res, next) {
-    institutionM.fetchNoUserInst(req, res);
+  app.get('/api/institution/fetch_number_teachers',function (req, res, next) { //enti e ruoli nei quali è iscritto l'utente
+    institutionM.fetchNumberTeachers(req, res);
   });
 
-  app.get('/api/institution/fetch_users_in_inst',function (req, res, next) {
+  app.get('/api/institution/fetch_users_in_inst',function (req, res, next) { 
     institutionM.fetchUsersInInst(req, res);
+  });
+
+  app.get('/api/institution/fetch_no_user_inst',function (req, res, next) { //enti nei quali non è l'utente
+    institutionM.fetchNoUserInst(req, res);
   });
 
   app.post('/api/institution/change_inst',function (req, res, next) {

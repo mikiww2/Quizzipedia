5c0cdf8de38736c6cfe11c79c009b245f7516da8
Bock92 <boccato92@gmail.com>
2016-06-13 11:56:43 +0200

    tolto commenti

--- public/question_creation/CtrlQuestion.js
+++ public/question_creation/CtrlQuestion.js
@@ -352,10 +352,7 @@ angular.module('CreateQuestion').controller('CtrlQuestion',['$scope','$http','Tr
                     }
                     
                 }
-            }
-            
-            
-            
+            }        
             
         }
                    

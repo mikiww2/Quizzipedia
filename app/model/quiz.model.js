f65b7fbad5e1a4ce1ad1605b855d1e1c1872908d
miki2w <mikiww2@gmail.com>
2016-06-13 09:55:20 +0200

    [model] commentate funzioni non utilizzate

--- app/model/quiz.model.js
+++ app/model/quiz.model.js
@@ -65,55 +65,55 @@ var quizSchema = new Schema({
     }
 }, { strict: true });
 
-//countQuestions
-quizSchema.virtual('countQuestions').get(function() {
-    this.questions.count({}, function(err, number) {
-        return number;
-    });
-});
-
-//Count Quiz with Question (scope = collection)
-quizSchema.statics.CountQuizWithQuestion = function(question) {
-    if(question.constructor.name !== 'ObjectID')
-        question = question._id;
-
-    return this.count({ questions: {$in: question} }, function(err, number) {
-        return number;
-    });
-};
-
-//find Quiz with topics (scope = collection)
-quizSchema.statics.findQuizByTopics = function(topics) {
-    var result = this.find();
-    topics.forEach(function(entry) {
-        result = result.find({ topics: {$in: entry}});
-    });
-    return result;
-};
-
-//find Quiz with keywords (scope = collection)
-quizSchema.statics.findQuizByKeywords = function(keywords) {
-    var result = this.find();
-    keywords.forEach(function(entry) {
-        result = result.find({ keywords: {$in: entry}});
-    });
-    return result;
-};
-
-//find Quiz with title (scope = collection)
-quizSchema.statics.findQuizByTitle = function(title) {
-  return this.find({ title: title });
-};
-
-//find Quiz with difficulty (scope = collection)
-quizSchema.statics.findQuizByDifficulty = function(difficulty) {
-  return this.find({ difficulty: difficulty });
-};
-
-//find Quiz with author (scope = collection)
-quizSchema.statics.findQuizsByAuthor = function(author) {
-  return this.find({ author: author });
-};
+// //countQuestions
+// quizSchema.virtual('countQuestions').get(function() {
+//     this.questions.count({}, function(err, number) {
+//         return number;
+//     });
+// });
+//
+// //Count Quiz with Question (scope = collection)
+// quizSchema.statics.CountQuizWithQuestion = function(question) {
+//     if(question.constructor.name !== 'ObjectID')
+//         question = question._id;
+//
+//     return this.count({ questions: {$in: question} }, function(err, number) {
+//         return number;
+//     });
+// };
+//
+// //find Quiz with topics (scope = collection)
+// quizSchema.statics.findQuizByTopics = function(topics) {
+//     var result = this.find();
+//     topics.forEach(function(entry) {
+//         result = result.find({ topics: {$in: entry}});
+//     });
+//     return result;
+// };
+//
+// //find Quiz with keywords (scope = collection)
+// quizSchema.statics.findQuizByKeywords = function(keywords) {
+//     var result = this.find();
+//     keywords.forEach(function(entry) {
+//         result = result.find({ keywords: {$in: entry}});
+//     });
+//     return result;
+// };
+//
+// //find Quiz with title (scope = collection)
+// quizSchema.statics.findQuizByTitle = function(title) {
+//   return this.find({ title: title });
+// };
+//
+// //find Quiz with difficulty (scope = collection)
+// quizSchema.statics.findQuizByDifficulty = function(difficulty) {
+//   return this.find({ difficulty: difficulty });
+// };
+//
+// //find Quiz with author (scope = collection)
+// quizSchema.statics.findQuizsByAuthor = function(author) {
+//   return this.find({ author: author });
+// };
 
 //export
 module.exports = mongoose.model('Quiz', quizSchema);
--- app/model/resultQuiz.model.js
+++ app/model/resultQuiz.model.js
@@ -47,121 +47,121 @@ var resultQuizSchema = new Schema({
     }
 }, { strict: true });
 
-//result, return [total, passed]
-resultQuizSchema.virtual('result').get(function () {
-    var result_q = 0;
-    var tot_q = 0;
-    
-    this.answers.forEach(function(entry) {
-        tot_q++;
-        if(entry.solution)
-            result_q++;
-    });
-    
-    return {
-        total: tot_q
-        ,passed: result_q
-    };
-});
+// //result, return [total, passed]
+// resultQuizSchema.virtual('result').get(function () {
+//     var result_q = 0;
+//     var tot_q = 0;
+//
+//     this.answers.forEach(function(entry) {
+//         tot_q++;
+//         if(entry.solution)
+//             result_q++;
+//     });
+//
+//     return {
+//         total: tot_q
+//         ,passed: result_q
+//     };
+// });
 
-//find results Question, return [total, passed] (scope = collection)
-resultQuizSchema.statics.findMeanResultsQuestion = function(question) {
-    if(question.constructor.name != 'ObjectID')
-        question = question._id;
-
-    var result_q = 0;
-    var tot_q = 0;
-    
-    this.find({}, 'answers', function(err, answers) {
-        answers.forEach(function(entry) {
-            entry.forEach(function(answer) {
-                if(answer.question == question) {
-                    tot_q++;
-
-                    if(answer.question == true)
-                        result_q++;
-                }
-            });
-        });
-    });
-    return {
-        total: tot_q
-        ,passed: result_q
-    };
-};
-
-//find Tot results Quiz, return number (scope = collection)
-resultQuizSchema.statics.findTotResultsQuiz = function(quiz) {
-    if (quiz.constructor.name !== 'ObjectID')
-        quiz = quiz._id;
-    this.count({ quiz: quiz }, function(err, number) {
-        return number;
-    });
-};
-
-//find mean results Quiz, return [total, passed] (scope = collection)
-resultQuizSchema.statics.findMeanResultsQuestion = function (quiz) {
-    if(quiz.constructor.name != 'ObjectID')
-        quiz = quiz._id;
-
-    var result_q = 0;
-    var tot_q = 0;
-
-    this.find({ quiz: quiz }, 'answers', function(err, answers) {
-        answers.forEach(function(entry) {
-            entry.forEach(function(answer) {
-                tot_q++;
-
-                if(answer.question == true)
-                    result_q++;
-            });
-        });
-    });
-
-    return {
-        total: tot_q
-        ,passed: result_q
-    };
-};
-
-//find Users mean Results Quiz, return [user, total, passed ] (scope = collection)
-resultQuizSchema.statics.findUsersMeanResultsQuiz = function(quiz) {
-    if(quiz.constructor.name != 'ObjectID')
-        quiz = quiz._id;
-
-    var results = {};
-    this.find({ quiz: quiz }, function(err, quizs) {
-        quizs.forEach(function(entry) {
-            var result_q = 0;
-            var tot_q = 0;
-
-            entry.answers.forEach(function(entry) {
-                entry.forEach(function(answer) {
-                    tot_q++;
-                    if(answer.question == true)
-                        result_q++;
-                });
-            });
-
-            results.push({
-                user: entry.user
-                ,total: tot_q
-                ,passed: result_q
-            });
-        });
-    });
-    return results;
-};
-
-//find resultQuiz with user
-resultQuizSchema.statics.findResultQuizByUser = function(user) {
-  return this.find({ user: user });
-};
-
-//find results with Quiz
-resultQuizSchema.statics.findResultQuizByUser = function(user) {
-  return this.find({ user: user });
-};
+// //find results Question, return [total, passed] (scope = collection)
+// resultQuizSchema.statics.findMeanResultsQuestion = function(question) {
+//     if(question.constructor.name != 'ObjectID')
+//         question = question._id;
+//
+//     var result_q = 0;
+//     var tot_q = 0;
+//
+//     this.find({}, 'answers', function(err, answers) {
+//         answers.forEach(function(entry) {
+//             entry.forEach(function(answer) {
+//                 if(answer.question == question) {
+//                     tot_q++;
+//
+//                     if(answer.question == true)
+//                         result_q++;
+//                 }
+//             });
+//         });
+//     });
+//     return {
+//         total: tot_q
+//         ,passed: result_q
+//     };
+// };
+//
+// //find Tot results Quiz, return number (scope = collection)
+// resultQuizSchema.statics.findTotResultsQuiz = function(quiz) {
+//     if (quiz.constructor.name !== 'ObjectID')
+//         quiz = quiz._id;
+//     this.count({ quiz: quiz }, function(err, number) {
+//         return number;
+//     });
+// };
+//
+// //find mean results Quiz, return [total, passed] (scope = collection)
+// resultQuizSchema.statics.findMeanResultsQuestion = function (quiz) {
+//     if(quiz.constructor.name != 'ObjectID')
+//         quiz = quiz._id;
+//
+//     var result_q = 0;
+//     var tot_q = 0;
+//
+//     this.find({ quiz: quiz }, 'answers', function(err, answers) {
+//         answers.forEach(function(entry) {
+//             entry.forEach(function(answer) {
+//                 tot_q++;
+//
+//                 if(answer.question == true)
+//                     result_q++;
+//             });
+//         });
+//     });
+//
+//     return {
+//         total: tot_q
+//         ,passed: result_q
+//     };
+// };
+//
+// //find Users mean Results Quiz, return [user, total, passed ] (scope = collection)
+// resultQuizSchema.statics.findUsersMeanResultsQuiz = function(quiz) {
+//     if(quiz.constructor.name != 'ObjectID')
+//         quiz = quiz._id;
+//
+//     var results = {};
+//     this.find({ quiz: quiz }, function(err, quizs) {
+//         quizs.forEach(function(entry) {
+//             var result_q = 0;
+//             var tot_q = 0;
+//
+//             entry.answers.forEach(function(entry) {
+//                 entry.forEach(function(answer) {
+//                     tot_q++;
+//                     if(answer.question == true)
+//                         result_q++;
+//                 });
+//             });
+//
+//             results.push({
+//                 user: entry.user
+//                 ,total: tot_q
+//                 ,passed: result_q
+//             });
+//         });
+//     });
+//     return results;
+// };
+//
+// //find resultQuiz with user
+// resultQuizSchema.statics.findResultQuizByUser = function(user) {
+//   return this.find({ user: user });
+// };
+//
+// //find results with Quiz
+// resultQuizSchema.statics.findResultQuizByUser = function(user) {
+//   return this.find({ user: user });
+// };
 
 //export
 module.exports = mongoose.model('ResultQuiz', resultQuizSchema);

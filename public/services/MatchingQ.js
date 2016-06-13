5da3e5e8b2a69cf15c58ac716240f39ae8cdf9a1
Bock92 <boccato92@gmail.com>
2016-06-13 11:59:35 +0200

    da modificare

--- public/services/MatchingQ.js
+++ public/services/MatchingQ.js
@@ -1,4 +1,4 @@
-angular.module().factory('MatchingQ',['Column',function(){
+angular.module().factory('MatchingQ',[function(){
     
 	/*
 	il testo deve essere un array con questi json dentro
@@ -10,27 +10,7 @@ angular.module().factory('MatchingQ',['Column',function(){
 	per gli allegati dimmi te, in teoria possono esserci sia nella domanda che risposta
 	*/
 
-    function MatchingQ(){
-        this.columns = []; //Column[]
-    };
     
-    MatchingQ.prototype.addColumn = function(newColumn){ //newColumn:Column
-      this.columns.push(newColumn);  
-    };
-    
-    MatchingQ.prototype.createAnswerQuestion = function(answerColumn){ //AnswerColumn[]
-        
-    };
-    
-    MatchingQ.prototype.setColumns = function (newColumn){ //newColumns:Column[]
-        
-    };
-    
-    MatchingQ.prototype.getColumns= function(){
-        return this.columns;
-    };
-    
-    return MatchingQ;
     
     
 }]);

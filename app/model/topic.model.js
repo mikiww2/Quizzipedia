5eb8c9b2f8a48f0edcaa685b32371dfb4eeb9a31
devsoft91 <interbelt@gmail.com>
2016-06-14 15:09:37 +0200

    [model] Aggiunto topic model

---
+++ app/model/topic.model.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//declare required
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//topic
+var topicSchema = new Schema({
+    name: {
+        type: String
+        ,lowercase: true
+        ,trim: true
+    }
+}, {
+    strict: true
+});
+
+//find topics
+topicSchema.statics.findTopics = function findTopics() {
+    return this.find({ });
+};
+
+//has topic
+topicSchema.statics.hasTopic = function hasTopic(topic) {
+    return !!this.findOne({_id: topic});
+};
+
+//export
+module.exports = mongoose.model('Topic', topicSchema); 

369ac6639a7802417fcaace101e2a878222ba0e9
Bock92 <boccato92@gmail.com>
2016-06-14 20:33:30 +0200

    collegato il controller al module QuizSolver

---
+++ public/quiz_execution/CtrlQuizExec.js
@@ -0,0 +1,24 @@
+angular.module('QuizSolver').controller('CtrlExecutionQuiz',[function(){
+    
+    
+    $scope.indexQuestion = 0;
+    $scope.question = null; //GenericQuestion
+    $scope.answerQuiz = new AnswerQuiz();
+    $scope.answer = null;
+    $scope.loadQuestion = function(indexQuestion){
+        //localmente recupero la domanda in posizione indexQuestion e la assegno a $scope.question.
+        //successivamente verifico in $scope.answerQuiz se esiste già una risposta in answerQuiz. Se si carico quella altrimenti ne creo una vuota e la si assegna a $scope.answer
+    };
+    
+    $scope.saveAnswer = function(){
+      //inserisce $scope.asnwer in $scope.answerQuiz nella posizione corretta  
+    };
+    
+    $scope.saveQuiz = function(){
+        
+        //salvo il quiz localmente dentro a $sope.user e poi invio la richesta al server
+    };
+    
+    
+    
+}]);

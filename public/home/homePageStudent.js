c473b9e15c37bf60488b50b8415c7ccdab154f72
devsoft91 <interbelt@gmail.com>
2016-06-15 23:00:52 +0200

    [client] Creato gestione homepage studente e quiz privati

--- public/home/homePageStudent.js
+++ public/home/homePageStudent.js
@@ -15,7 +15,19 @@ angular.module('Quizzipedia').directive('homePageStudent',function(){
     
     return {
       restrict: 'E',
-      templateUrl: './public/home/homepageStudent.html'
+      templateUrl: './public/home/homepageStudent.html',
+      controller: function ($scope, $http) {
+          
+          $scope.userClasses = [];
+          
+          $scope.fetchClassesWithQuiz = function () {
+              $http.get('/api/class/fetch_classes_with_quiz').success(function(response) {
+                  $scope.userClasses = response;
+              });
+          }
+          
+          $scope.fetchClassesWithQuiz();
+        }
     };
     
 });

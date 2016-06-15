cc52ffc8ef47a28afe76ff3040ef64e422a5c123
miki2w <mikiww2@gmail.com>
2016-06-15 12:34:06 +0200

    [model] pulito il codice dei model

--- app/model/quiz.model.js
+++ app/model/quiz.model.js
@@ -5,7 +5,7 @@
  * Data creazione:
  * E-mail: vaulttech.swe@gmail.com
  *
- *  Model che rappresenta i quiz nel database e vari metodi associati
+ *  Model che rappresenta i quiz nel database
  *
  * * Diario delle modifiche:
  *
@@ -64,56 +64,6 @@ var quizSchema = new Schema({
         ,required: [true, 'institution name is required']
     }
 }, { strict: true });
-
-// //countQuestions
-// quizSchema.virtual('countQuestions').get(function() {
-//     this.questions.count({}, function(err, number) {
-//         return number;
-//     });
-// });
-//
-// //Count Quiz with Question (scope = collection)
-// quizSchema.statics.CountQuizWithQuestion = function(question) {
-//     if(question.constructor.name !== 'ObjectID')
-//         question = question._id;
-//
-//     return this.count({ questions: {$in: question} }, function(err, number) {
-//         return number;
-//     });
-// };
-//
-// //find Quiz with topics (scope = collection)
-// quizSchema.statics.findQuizByTopics = function(topics) {
-//     var result = this.find();
-//     topics.forEach(function(entry) {
-//         result = result.find({ topics: {$in: entry}});
-//     });
-//     return result;
-// };
-//
-// //find Quiz with keywords (scope = collection)
-// quizSchema.statics.findQuizByKeywords = function(keywords) {
-//     var result = this.find();
-//     keywords.forEach(function(entry) {
-//         result = result.find({ keywords: {$in: entry}});
-//     });
-//     return result;
-// };
-//
-// //find Quiz with title (scope = collection)
-// quizSchema.statics.findQuizByTitle = function(title) {
-//   return this.find({ title: title });
-// };
-//
-// //find Quiz with difficulty (scope = collection)
-// quizSchema.statics.findQuizByDifficulty = function(difficulty) {
-//   return this.find({ difficulty: difficulty });
-// };
-//
-// //find Quiz with author (scope = collection)
-// quizSchema.statics.findQuizsByAuthor = function(author) {
-//   return this.find({ author: author });
-// };
 
 //export
 module.exports = mongoose.model('Quiz', quizSchema);

2d7bd385c9c58f741c0ed488cd8f99236ace224d
BikuM <vassiliki.menarin@gmail.com>
2016-06-15 17:31:33 +0200

    [client]Sistemato upload domande v/f

--- public/core.js
+++ public/core.js
@@ -25,8 +25,6 @@ angular.module('TopicsManager', ['Quizzipedia']);
 angular.module('UsersManager', ['Quizzipedia']);
 angular.module('SearchManager', ['Quizzipedia']);
 angular.module('QuizManager', ['Quizzipedia', 'ui.bootstrap']);
-angular.module('QuizSolver', ['Quizzipedia','CreateQuestion']).config(function($sceProvider) {
-    $sceProvider.enabled(false);
-});
+angular.module('QuizSolver', ['Quizzipedia','CreateQuestion']);
 
 angular.module('InstitutionManager', ['Quizzipedia']);

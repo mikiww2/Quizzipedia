f3be4617fc04acaea2a65946cab951f1872f7a0f
BikuM <vassiliki.menarin@gmail.com>
2016-06-16 01:17:50 +0200

    [client] Fix header

--- public/header/headerNotLogged.js
+++ public/header/headerNotLogged.js
@@ -15,7 +15,29 @@ angular.module('Quizzipedia').directive('headerNotLogged',function(){
     
     return{
         restrict: 'E',
-        templateUrl: './public/header/header_notLogged.html'
+        templateUrl: './public/header/header_notLogged.html',
+        controller: function ($scope, $http, $window) {
+            
+            $scope.email = null;
+            $scope.password = null;
+            
+            $scope.signin = function(){
+                var user = {
+                    email: $scope.email,
+                    password: $scope.password
+                };
+                $http.post('/api/auth/signin', user)
+                    .success(function(response){
+                    if(response.code == 0)
+                        $window.location.href='/Quizzipedia/home';
+                    if(response.code == 1)
+                        alert(response.message);
+                }).error(function(response){
+                    alert("Errore");
+                });
+            };
+        }
+        
     };
     
     

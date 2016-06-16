24860edd7a63c1255a79dcebc29fb1ac4576fd64
BikuM <vassiliki.menarin@gmail.com>
2016-06-16 11:09:52 +0200

    [client]Sistemta homepage generica

--- public/home/homePageGeneric.js
+++ public/home/homePageGeneric.js
@@ -11,9 +11,41 @@
  *
  */
 
-angular.module('Quizzipedia').directive('homePageGeneric',function(){
+angular.module('Quizzipedia').directive('homePageGeneric', function(){
     return {
         restrict: 'E',
-        templateUrl: './public/home/homepageGeneric.html'
-    };
+        templateUrl: './public/home/homepageGeneric.html',
+        controller: function ($scope, $http, $window) {
+            $scope.recentQuizzes = [];
+            
+            $scope.loadQuizzes = function () {
+                
+                var searchQ = {title : null, author : null, topic : null, keyword : null, difficulty : null};
+                
+                $http.post('/api/quiz/search', searchQ).success(function(response) {
+                    $scope.recentQuizzes = response;
+                    
+                    angular.forEach ($scope.recentQuizzes, function(quiz) {
+                        if (quiz.difficulty == 1)
+                            quiz.difficulty = "Facile";
+                        else if (quiz.difficulty == 2)
+                            quiz.difficulty = "Medio";
+                        else if (quiz.difficulty == 3)
+                            quiz.difficulty = "Difficile";
+                        else if (quiz.difficulty == 4)
+                            quiz.difficulty = "Molto difficile";
+                    });
+                });
+            };
+            
+            $scope.selectQuiz = function(quizToSolve) {
+                $http.post('/api/quiz/prepare_quiz_execution', quizToSolve)
+                    .success(function(response) {
+                    $window.location.href = '/Quizzipedia/quizExec';
+                });
+            };
+            
+            $scope.loadQuizzes();            
+        }
+    }
 });

41d52f9d6f86af5f66a3bfbfeb9483c824846543
BikuM <vassiliki.menarin@gmail.com>
2016-06-16 23:35:10 +0200

    [client] Storico quiz

--- public/quiz_execution/CtrlQuizHistory.js
+++ public/quiz_execution/CtrlQuizHistory.js
@@ -14,35 +14,21 @@
 angular.module('QuizManager').controller('CtrlQuizHistory',['$scope','$http', function($scope, $http){
     
     $scope.userQuizzes = [];
-    $scope.quizResult = {rispTot : null, rispCorr : null, esito : null};
     
     $scope.loadUserQuizzes = function(){
-            $http.get('/api/get_solved_user_quiz').success(function(response){
-            $scope.quiz = response;
+            $http.get('/api/quiz/get_solved_user_quiz').success(function(response){
+            $scope.userQuizzes = response;
+            console.log(response);
         });
         
     };
     
-    $scope.loadResults = function(indexOfQuiz) {
-        //Per un quiz conta: risposte tot, risposte  giuste, superamento e mette in quizResult
-        
-        var quizSelected = $scope.userQuizzes[indexOfQuiz];
-        var totali = quizSelected.answers.length;
-        var corrette = 0;
-        var passato = false;
-        
-        angular.forEach(quizSelected.answers, function(answer) {
-            if (answer === true)
-                corrette=corrette +1;
-        });
-        
-        var percentuale = (corrette * 100) / totali;
-        
-        if (percentuale >= 60) 
-            passato = true;            
-        
-        $scope.quizResult = {rispTot : totali, rispCorr : corrette, esito : passato};      
-    }; 
+    $scope.success = function (percentage) {
+        if (percentage >= 60)
+            return true;
+        else
+            return false;
+    };
     
     $scope.loadUserQuizzes();
     
